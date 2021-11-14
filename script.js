9e3c3956faaaa8d8457b426aefd87fcc0673d5c3
thegjm <thegjmercedes@gmail.com>
2021-11-14 02:13:54 -0500

    updated js file to reset fields when an principal contains an invalid value

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function compute(){
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     
-    
     /*This is a much nicer way of formatting strings:
     the quote above ~ on keyboard-> `String around the ${variable} and more string ${otherJS}` rinse and repeat */
     document.getElementById("result").innerHTML=`If you deposit <span class="highlight">${principal}</span>,<br/>
@@ -14,6 +13,7 @@ function compute(){
     You will receive an amount of <span class="highlight">${interest}</span>,<br/>
     in the year <span class="highlight">${year}</span>`;
     //also, changed innerText to innerHTML
+
 }
 
 //updates the html for Rates
@@ -29,6 +29,13 @@ function checkValue() {
     //placing focus on principal input if any one condition is true
     if((amount == "") || (amount == undefined) || (amount == NaN) || (amount <= 0)){
         alert("Enter a positive number");
+        //resets values on the page if the principal is an invalid value
+        document.getElementById("result").innerText = "";
+        document.getElementById("principal").value = "";
+        document.getElementById("rate").value = 10.25;
+        updateRate();
+        document.getElementById("years").selectedIndex = 0;
+        //sets focus on the principal input box
         principal.focus();
         return false;
     }else{
